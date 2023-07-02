683609faa41e3e5339fd675a1f1cd72820a2f436
TheGiantNinja <yasserfedsi34@gmail.com>
2023-07-02 18:41:10 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
   theme: {
     fontFamily: {
       Roboto: ["Roboto", "sans-serif"],
@@ -8,9 +9,9 @@ module.exports = {
     },
     extend: {
       screens: {
-        "100px": "1050px",
+        "1000px": "1050px",
         "1100px": "1110px",
-        "808px": "880px",
+        "800px": "800px",
         "1300px": "1300px",
         "400px": "400px",
       },
