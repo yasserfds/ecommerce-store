e36f10f15628e2028370f8b14a60760b2f8adc06
TheGiantNinja <yasserfedsi34@gmail.com>
2023-07-03 02:10:54 +0200

    Login section is done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Login from "../components/Login/Login.jsx";
 
 const LoginPage = () => {
     return (
-        <div className="w-full h-screen bg-gray-50">
+        <div>
             <Login />
         </div>
     )
