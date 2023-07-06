9bc0c6784ca373754c427578a37648eaf11556d3
TheGiantNinja <yasserfedsi34@gmail.com>
2023-07-07 00:53:00 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+const express = require('express');
+const ErrorHandler = require('./utils/ErrorHandler');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fileUpload = require("express-fileupload");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({
-        path: "backend/config/.env"
-    })
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
 
-//it's for Error Handling
+// import routes
+const user = require("./controller/user");
+
+app.use('api/v2/user', user);
+
+// It's for ErrorHandling
 app.use(ErrorHandler);
 
 module.exports = app;
