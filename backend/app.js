f010220903644e784ea69fce8103bbcbd3a694d5
TheGiantNinja <yasserfedsi34@gmail.com>
2023-07-17 23:40:50 +0200

    Added cors lib

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
