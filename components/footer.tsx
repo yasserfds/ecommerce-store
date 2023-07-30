61216debba26bc4d9b39180f6707a374e7b45ea2
TheGiantNinja <yasserfedsi34@gmail.com>
2023-07-31 00:46:54 +0200

    Added footer

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 AR Store - All right reserved. By Yasser Fedsi
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
