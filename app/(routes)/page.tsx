ddb8c2583770573600f13bd2b2135e4d26eec4b6
TheGiantNinja <yasserfedsi34@gmail.com>
2023-08-08 22:13:40 +0200

    Product section is done

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,24 @@
 import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("ff0f5e55-452e-4f95-a86c-cf779a05a754");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard}/>
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
 export default HomePage;
